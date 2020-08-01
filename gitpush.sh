3c5bab0b3b363666537153169862b64bd48e64b0
xiaodongli1986 <xiaodongli@mail.kias.re.kr>
2020-08-01 20:51:32 +0800

    minor changes

--- gitpush.sh
+++ gitpush.sh
@@ -3,6 +3,7 @@ git add *.sh
 git add README.md
 git add -f cola_halo_lc/*.c
 git add -f cola_halo_lc/*.h
+git add -f cola_halo_lc/Makefile
 git add cola_halo_lc/tools/*
 git commit -m 'minor changes'
 git push
